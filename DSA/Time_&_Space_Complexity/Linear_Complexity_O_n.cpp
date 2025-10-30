e0f1f242bff0dd9e9fa5dee91e26c6a136c23408
Mostasir Mahim <mahimlive@gmail.com>
2025-10-30 23:05:03 +0600

    dsa(time & space complexity) : linear complexity

---
+++ DSA/Time_&_Space_Complexity/Linear_Complexity_O_n.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;//O(1)
+    cin >> n; //O(1)
+
+    for (int i=1;i<=n;i++) { //O(n)
+        cout << i << " ";
+    }
+        //OUTPUT : 1 2 3 4 5
+        //So Depends on n & Its worst case so its final complexity
+    return 0; //O(1)
+}
